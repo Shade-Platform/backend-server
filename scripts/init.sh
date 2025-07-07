73d76ea679a3f2fb7c6b60f2243ada9e3eedbb4e
54771059 <darkvalvet@protonmail.com>
2025-07-07 16:11:47 +0300

    juices up minikube

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Convenience script for quick startup
-minikube start # Start the minikube cluster first
+minikube start --cpus=4 --memory=8192 --disk-size=20g # Start the minikube cluster first
 docker compose up --build -d # Build the image and run it
 docker exec -it shade_server go run main.go migrate up # Start the migrations
